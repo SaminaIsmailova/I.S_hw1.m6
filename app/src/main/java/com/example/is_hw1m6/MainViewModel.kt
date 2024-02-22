618b019da9bb26dea2856d100a343aa6fa08fa6b
SaminaIsmailova <samina.ismailova.06@gmail.com>
2024-02-22 13:50:36 +0600

    Initial commit

---
+++ app/src/main/java/com/example/is_hw1m6/MainViewModel.kt
@@ -0,0 +1,23 @@
+package com.example.is_hw1m6
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+
+class MainViewModel : ViewModel() {
+
+    private var count = 0
+    private val _counterLv = MutableLiveData<Int>()
+    val counterLv: LiveData<Int>
+        get() = _counterLv
+
+    fun inc() {
+        _counterLv.value = count++
+    }
+
+    fun dec() {
+        _counterLv.value = count--
+    }
+
+}
